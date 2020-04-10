d635ac99aa20fa7d059272555946af1cecc4a28b
SeanMcelroy97 <seanpetermcelroy@gmail.com>
2020-04-10 13:52:39 +0100

    Added an appointment Obj. with Event, Additional Info, Time

--- app/src/main/java/com/example/patientpal/model/Appointment.java
+++ app/src/main/java/com/example/patientpal/model/Appointment.java
@@ -1,4 +1,38 @@
 package com.example.patientpal.model;
 
+
+import com.github.sundeepk.compactcalendarview.domain.Event;
+
+import java.util.Date;
+
 public class Appointment {
+
+    private Event calendarEvent;
+    private String additionalInfo;
+    private Date time;
+
+
+    public Event getCalendarEvent() {
+        return calendarEvent;
+    }
+
+    public void setCalendarEvent(Event calendarEvent) {
+        this.calendarEvent = calendarEvent;
+    }
+
+    public String getAdditionalInfo() {
+        return additionalInfo;
+    }
+
+    public void setAdditionalInfo(String additionalInfo) {
+        this.additionalInfo = additionalInfo;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
 }
